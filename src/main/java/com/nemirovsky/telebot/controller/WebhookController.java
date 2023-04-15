42fc723355f864c39ff8fcb6710507f2d8d2d399
Stanislav Nemirovsky <choonsky@gmail.com>
2023-04-15 14:32:48 +0600

    Fix 11

--- src/main/java/com/nemirovsky/telebot/controller/WebhookController.java
+++ src/main/java/com/nemirovsky/telebot/controller/WebhookController.java
@@ -1,7 +1,6 @@
 package com.nemirovsky.telebot.controller;
 
 import com.nemirovsky.telebot.model.TelegramBot;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
