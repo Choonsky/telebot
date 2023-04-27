61c229a3086f20d71f3ce50662d8c9c4c93148a7
Stanislav Nemirovsky <choonsky@gmail.com>
2023-04-27 17:27:51 +0600

    Added user recognition

--- src/main/java/com/nemirovsky/telebot/controller/WebhookController.java
+++ src/main/java/com/nemirovsky/telebot/controller/WebhookController.java
@@ -10,6 +10,7 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
 import java.util.Enumeration;
 
@@ -24,21 +25,63 @@ public class WebhookController {
 
 // point for message
     @PostMapping("/")
-    public BotApiMethod<?> nUpdateReceived(@RequestBody Update update) {
+    public BotApiMethod<?> updateReceived(@RequestBody Update update) {
+
+        Message msg = update.getMessage();
+        User user = msg.getFrom();
 
         //TODO: del
         System.out.println("Incoming POST request: " + update);
+        String text;
 
-        Message message = update.getMessage();
-        long userId = message.getFrom().getId();
-        long chatId = message.getChatId();
+        if ("/start".equals(msg.getText())) {
+            text =
+                    "Добро пожаловать на сайт по отлову, тьфу, поиску домашних животных и их владельцев, "
+                           + user.getFirstName() + " " + user.getLastName() + " по кличке " + user.getUserName()
+                            + "! Мы определили ваш язык как " + user.getLanguageCode() + ". Выберите кнопку или " +
+                            "введите текст запроса или команду...";
+        } else {
+            text = "Вы ввели " + msg.getText() + ", " + user.getUserName() + "!";
+        }
+        long userId = msg.getFrom().getId();
+        long chatId = msg.getChatId();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
-        sendMessage.setText("И что же вы, юзер " + userId + ", имеете в виду под \"" + message.getText() + "\"?");
+        sendMessage.setText(text);
         return sendMessage;
 
         //return telegramBot.onWebhookUpdateReceived(update);
     }
+
+//    try {
+//        SendPhoto sendPhoto = new SendPhoto()
+//                .setChatId(update.getMessage().getChatId())
+//                .setPhoto("Photo", new FileInputStream(new File("/root/index.png")));
+//        execute(sendPhoto);
+//    } catch (FileNotFoundException e) {
+//        e.printStackTrace();
+//    } catch (TelegramApiException e) {
+//        e.printStackTrace();
+//    }
+
+
+//    public void sendPhotoMessage(long chatId, UserContent content) {
+//
+//        String caption = getCaption(content);
+//
+//        SendPhoto sendPhoto = SendPhoto.builder()
+//                .chatId(chatId)
+//                .photo(new InputFile(new File(content.getMediaUrl())))
+//                .caption(caption)
+//                .parseMode(ParseMode.HTML)
+//                .build();
+//
+//        try {
+//            chronologyBot.execute(sendPhoto);
+//        } catch (TelegramApiException e) {
+//            log.error("Can't send photo message", e);
+//        }
+//    }
 
     @GetMapping("/")
     public String get(HttpServletRequest request) {
